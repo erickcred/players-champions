927980efa94cb3aa5130d824df9162a26e9835c5
Erick Rick <erickcredjes@gmail.com>
2025-07-11 19:49:53 -0300

    aplicando cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express, { json } from "express";
+import cors from "cors";
 import router from "./router/routes";
 
 function createApp() {
   const app = express();
   
   app.use(json());
+  app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }));
   
   app.use("/api", router);
 
