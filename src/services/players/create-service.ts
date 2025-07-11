72be31eafb4930eae17470d28e21995dbcfe52d4
Erick Rick <erickcredjes@gmail.com>
2025-07-11 09:41:09 -0300

    limpando console

--- src/services/players/create-service.ts
+++ src/services/players/create-service.ts
@@ -3,9 +3,6 @@ import { badRequest, created } from "../../utils/http-helpers/http-status-code";
 
 export const createPlayerService = async (playerRequest: any) => {
   const response = await createPlayerReposiroty(playerRequest);
-  console.log("---------")
-  console.log(response)
-  console.log("--------")
 
   if (response)
     return created(response);
